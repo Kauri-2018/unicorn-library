bada937480cbd2f6e086c5b9f0843dd18aae348a
Frank <xuzy213@gmail.com>
2018-04-13 09:52:04 +1200

    add ToRead comp

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
 import ToRead from './ToRead'
-import Reading from './Reading'
-import Read from './Read'
-import Like from './Like'
+// import Reading from './Reading'
+// import Read from './Read'
+// import Like from './Like'
 
 const App = () => {
   return (
@@ -13,9 +13,9 @@ const App = () => {
         <h1>Unicorn library</h1>
         <div className='home'>
           <Route path='/' component={ToRead}/>
-          <Route exact path='/' component={Reading}/>
+          {/* <Route exact path='/' component={Reading}/>
           <Route path='/list/:rank' component={Read}/>
-          <Route path='/rank/:rank/:name' component={Like}/>
+          <Route path='/rank/:rank/:name' component={Like}/> */}
         </div>
       </div>
     </Router>

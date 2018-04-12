8ad6b27edecb5fd641e295120c707c345c426209
meakommo <meako98@gmail.com>
2018-04-13 10:08:19 +1200

    added css files and linked them to index.html

--- client/components/AddBook.jsx
+++ client/components/AddBook.jsx
@@ -1,5 +1,7 @@
 import react from 'React'
 
+
+
 class AddBook extends React.Component {
     constructor (props){
         super(props)
@@ -8,7 +10,12 @@ class AddBook extends React.Component {
         return (
             <div className="addbook">
                 <form>
-                    
+                Title: <input name='title' onchange={this.handleChange} />
+                Author: <input name='author' onchange={this.handleChange} />
+                {/* THis needs to be a dropdown menu below */}
+                Status <input name='status' onchange={this.handleChange} />
+                Title: <input name='title' onchange={this.handleChange} />
+
                 </form>
             </div>
         )

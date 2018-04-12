891a32aad1c1d32f34a3cbd80fc897ddc4a6baad
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-04-13 08:46:57 +1200

    initial setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+import ToRead from './ToRead'
+import Reading from './Reading'
+import Read from './Read'
+import Like from './Like'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div className='app'>
+        <h1>Unicorn library</h1>
+        <div className='home'>
+          <Route path='/' component={ToRead}/>
+          <Route exact path='/' component={Reading}/>
+          <Route path='/list/:rank' component={Read}/>
+          <Route path='/rank/:rank/:name' component={Like}/>
+        </div>
+      </div>
+    </Router>
   )
 }
 
 export default App
-

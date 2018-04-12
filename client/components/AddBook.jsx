a4a438966846df8e4d3388eb9b16d5b19b215576
meakommo <meako98@gmail.com>
2018-04-13 09:53:37 +1200

    started working on addbook form

---
+++ client/components/AddBook.jsx
@@ -0,0 +1,16 @@
+import react from 'React'
+
+class AddBook extends React.Component {
+    constructor (props){
+        super(props)
+    }
+    render() {
+        return (
+            <div className="addbook">
+                <form>
+                    
+                </form>
+            </div>
+        )
+    }
+}

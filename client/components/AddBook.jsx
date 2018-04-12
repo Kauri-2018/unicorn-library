489fb0d434b2a053d946d29bed93204f90160e2b
meakommo <meako98@gmail.com>
2018-04-13 10:19:46 +1200

    updated form and added more css

--- client/components/AddBook.jsx
+++ client/components/AddBook.jsx
@@ -10,12 +10,18 @@ class AddBook extends React.Component {
         return (
             <div className="addbook">
                 <form>
-                Title: <input name='title' onchange={this.handleChange} />
-                Author: <input name='author' onchange={this.handleChange} />
-                {/* THis needs to be a dropdown menu below */}
-                Status <input name='status' onchange={this.handleChange} />
-                Title: <input name='title' onchange={this.handleChange} />
-
+                    Title: <input name='title' onchange={this.handleChange} />
+                    Author: <input name='author' onchange={this.handleChange} />
+                    <div class="dropdown">
+                        <button class="btn btn-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Status
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a className="dropdown-item" name="1">To Read</a>
+                            <a className="dropdown-item" name="2">Reading</a>
+                            <a className="dropdown-item" name="3">Read</a>
+                        </div>
+                    </div>
                 </form>
             </div>
         )

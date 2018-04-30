c0d90a9cdcc80b4cdf4c9f5ea76298a3ea5eafc6
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-04-30 15:38:23 +1200

    I have no idea, dont ask

---
+++ server/routes/register.js
@@ -0,0 +1,7 @@
+// const express = require('express')
+
+// const {userExists, createUser} = require('../db/users')
+
+// const router = express.Router()
+
+// router.post('/register', register)
--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const path = require('path')
 const express = require('express')
+// const passport = require('passport')
+
+const authRoutes = require('./routes/auth')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+// server.use(passport.initialize())
+
+server.use('/api/v1/auth', authRoutes)
 
 module.exports = server
